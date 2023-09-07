1c5ed75038a9a9aa69b9ff113961ca3b042be530
m.backi <backimichal@gmail.com>
2023-09-07 10:52:00 +0200

    calc

---
+++ calculator/Components/ButtonBox.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../css/buttonbox.css";
+const ButtonBox = ({ children }) => {
+  return <div className="buttonbox">{children}</div>;
+};
+export default ButtonBox;
---
+++ calculator/Components/Screen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../css/screen.css";
+
+const Screen = ({ value }) => {
+  return <div className="screen">{value}</div>;
+};
+export default Screen;
